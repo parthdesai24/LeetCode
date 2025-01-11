de527f9b52901d41bce8df19295d7a4fba699bf4
Parth Desai <130673931+parthdesai24@users.noreply.github.com>
2025-01-11 21:31:02 +0530

    Time: 4 ms (26.3%), Space: 13.6 MB (15.64%) - LeetHub

--- 0935-orderly-queue/0935-orderly-queue.cpp
+++ 0935-orderly-queue/0935-orderly-queue.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     string orderlyQueue(string s, int k) {
-         if(k > 1) {
+        if (k > 1) {
             sort(begin(s), end(s));
             return s;
         }
-        
+
         string result = s;
-        for(int i = 1; i<s.length(); i++) {
+        int n = s.length();
+
+        for(int i = 1; i < n; i++){
             string temp = s.substr(i) + s.substr(0, i);
-            result = min(result, temp);
+            result = min(result,temp);
         }
-        
         return result;
     }
 };
