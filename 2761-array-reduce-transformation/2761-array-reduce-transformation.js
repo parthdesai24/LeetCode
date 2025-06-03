3dbec96d710c3b81ccec58d8b8fc270da54ae82d
Parth Desai <130673931+parthdesai24@users.noreply.github.com>
2025-06-03 14:25:01 +0530

    Time: 38 ms (85.18%), Space: 53.7 MB (90.24%) - LeetHub

--- 2761-array-reduce-transformation/2761-array-reduce-transformation.js
+++ 2761-array-reduce-transformation/2761-array-reduce-transformation.js
@@ -6,7 +6,6 @@
  */
 var reduce = function(nums, fn, init) {
     let result = init;
-    if(nums.length === 0) return init;
     for(let i = 0; i < nums.length; i++){
         result = fn(result,nums[i]);
     }
