38af36a58fed01a0681f79676ca4d656202c6197
Parth Desai <130673931+parthdesai24@users.noreply.github.com>
2025-06-05 15:11:52 +0530

    Time: 69 ms (15.54%), Space: 54.3 MB (65.05%) - LeetHub

---
+++ 2807-execute-asynchronous-functions-in-parallel/2807-execute-asynchronous-functions-in-parallel.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function(functions) {
+    return new Promise((resolve,reject) => {
+        let res = Array(functions.length);
+        let waitingFor = functions.length;
+        for(let i = 0; i < functions.length; i++){
+            functions[i]().then((result) => {
+                res[i] = result;
+                waitingFor--;
+                if(waitingFor === 0) resolve(res);
+            }).catch(reject)
+        }
+    })
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
