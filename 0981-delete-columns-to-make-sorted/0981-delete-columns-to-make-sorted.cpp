449c8f238e83154ae3ef0da9ca7e0dbe0185f0d2
Parth Desai <130673931+parthdesai24@users.noreply.github.com>
2025-06-15 12:30:08 +0530

    Time: 11 ms (5.43%), Space: 15.6 MB (45.16%) - LeetHub

--- 0981-delete-columns-to-make-sorted/0981-delete-columns-to-make-sorted.cpp
+++ 0981-delete-columns-to-make-sorted/0981-delete-columns-to-make-sorted.cpp
@@ -5,9 +5,9 @@ public:
         int k = strs[0].length();
         int count = 0;
 
-        for(int i = 0; i < k; i++){
-            for(int j = 1; j < n; j++){
-                if(strs[j][i] < strs[j-1][i]){
+        for (int i = 0; i < k; i++) {
+            for (int j = 1; j < n; j++) {
+                if (strs[j][i] < strs[j - 1][i]) {
                     count++;
                     break;
                 }
